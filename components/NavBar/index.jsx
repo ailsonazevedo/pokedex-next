ed17ad36362868ad62230bf2b33dc6c39945a5b8
ailsonazevedo <61508532+ailsonazevedo@users.noreply.github.com>
2022-06-20 10:35:09 -0300

    refactor: adjustment import css navbar

--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import styles from "./NavBar.module.css";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
